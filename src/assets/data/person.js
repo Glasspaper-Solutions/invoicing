f8fbb5d48aa8e0021ab951796252627d1b23429b
Glenn Nytveit <glenn.nytveit@telenor.no>
2023-02-13 12:34:42 +0100

    Added files and removed boilerplate

---
+++ src/assets/data/person.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'Bjarne Betjent',
+    role: 'user',
+    companies: [
+        {
+            id: '123476823asd',
+            name: 'Kims As',
+            applications: [
+                'coverage',
+                'salary',
+                'invoice'
+            ]
+        },
+        {
+            id: '12jhksadjfks3asd',
+            name: 'Maarud As',
+            applications: [
+                'coverage',
+                'salary'            ]
+        },
+        {
+            id: '12jhksadjfks3asd',
+            name: 'Sørlandschips As',
+            applications: [
+                'salary'            ]
+        }
+    ]
+}
