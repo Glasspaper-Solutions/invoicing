398f9a4a9a7c98881eda5d2489c09fde85f922fb
Lucas Bateson <lucas.bateson@gmail.com>
2023-02-13 13:21:52 +0100

    Styling fixes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
   <div class="container">
-    <div class="columns">
-      <div class="inputs">
-        <!--content-->
-      </div>
-      <div class="uploads">
-        <!--content-->
-      </div>
-      <div class="log">
-        <!--content-->
-      </div>
+    <div class="column inputs">
+      <!--content-->
+    </div>
+    <div class="column uploads">
+      <!--content-->
+    </div>
+    <div class="column log">
+      <!--content-->
     </div>
   </div>
 </template>
